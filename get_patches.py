8630af16d23d28165546ed64fd9a410df9c6047a
PeterB <byfield554@gmail.com>
2017-11-18 12:56:18 +0000

    save as png

--- get_patches.py
+++ get_patches.py
@@ -14,7 +14,7 @@ prefixes = {'Benign': 'b', 'InSitu': 'is', 'Invasive': 'iv', 'Normal': 'n'}
 
 def read_image(path):
     im = cv.imread(path)
-    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    # im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
     # im = im.astype(np.float32) / 255.0
     return im
 
@@ -29,62 +29,62 @@ def i2str(i):
         return s
 
 
-def get_patches(image):
-    stack = np.zeros((35 * 8, 512, 512, 3), dtype=np.float32)
-    counter = 0
-    for i in range(5):
-        for j in range(7):
-            patch = image[i * 256:i * 256 + 512, j * 256:j * 256 + 512, :]
-            stack[counter] = patch
-            counter += 1
-
-            patch_flip = cv.flip(patch, 1)
-            stack[counter] = patch_flip
-            counter += 1
-
-            m1 = cv.getRotationMatrix2D((256, 256), 90, 1)
-            rot1 = cv.warpAffine(patch, m1, (512, 512))
-            stack[counter] = rot1
-            counter += 1
-
-            rot1_flip = cv.flip(rot1, 1)
-            stack[counter] = rot1_flip
-            counter += 1
-
-            m2 = cv.getRotationMatrix2D((256, 256), 180, 1)
-            rot2 = cv.warpAffine(patch, m2, (512, 512))
-            stack[counter] = rot2
-            counter += 1
-
-            rot2_flip = cv.flip(rot2, 1)
-            stack[counter] = rot2_flip
-            counter += 1
-
-            m3 = cv.getRotationMatrix2D((256, 256), 270, 1)
-            rot3 = cv.warpAffine(patch, m3, (512, 512))
-            stack[counter] = rot3
-            counter += 1
-
-            rot3_flip = cv.flip(rot3, 1)
-            stack[counter] = rot3_flip
-            counter += 1
-
-    return stack
-
-
 #############################################
 
 
 for c in classes:
+    save_dir = os.path.join(os.getcwd(), 'patches', c)
+    os.makedirs(save_dir, exist_ok=True)
+
     for i in range(100):
         filename = prefixes[c] + i2str(i + 1) + '.tif'
         print('Doing Image {}'.format(filename))
         path = os.path.join(data_dir, c, filename)
         image = read_image(path)
 
-        patches = get_patches(image)
+        cnt = 1
+        for h in range(5):
+            for w in range(7):
+                patch = image[h * 256:h * 256 + 512, w * 256:w * 256 + 512, :]
+                save_filename = prefixes[c] + i2str(i + 1) + '_p' + i2str(cnt) + '.png'
+                cv.imwrite(os.path.join(save_dir,save_filename),patch)
+                cnt+=1
 
-        save_filename = prefixes[c] + i2str(i + 1) + '_patches.npy'
-        save_path = os.path.join(os.getcwd(), 'patches', c, save_filename)
-        os.makedirs(os.path.dirname(save_path), exist_ok=True)
-        np.save(save_path, patches)
+                patch_flip = cv.flip(patch, 1)
+                save_filename = prefixes[c] + i2str(i + 1) + '_p' + i2str(cnt) + '.png'
+                cv.imwrite(os.path.join(save_dir,save_filename),patch_flip)
+                cnt+=1
+
+                m1 = cv.getRotationMatrix2D((256, 256), 90, 1)
+                rot1 = cv.warpAffine(patch, m1, (512, 512))
+                save_filename = prefixes[c] + i2str(i + 1) + '_p' + i2str(cnt) + '.png'
+                cv.imwrite(os.path.join(save_dir,save_filename),rot1)
+                cnt+=1
+
+                rot1_flip = cv.flip(rot1, 1)
+                save_filename = prefixes[c] + i2str(i + 1) + '_p' + i2str(cnt) + '.png'
+                cv.imwrite(os.path.join(save_dir,save_filename),rot1_flip)
+                cnt+=1
+
+                m2 = cv.getRotationMatrix2D((256, 256), 180, 1)
+                rot2 = cv.warpAffine(patch, m2, (512, 512))
+                save_filename = prefixes[c] + i2str(i + 1) + '_p' + i2str(cnt) + '.png'
+                cv.imwrite(os.path.join(save_dir,save_filename),rot2)
+                cnt+=1
+
+                rot2_flip = cv.flip(rot2, 1)
+                save_filename = prefixes[c] + i2str(i + 1) + '_p' + i2str(cnt) + '.png'
+                cv.imwrite(os.path.join(save_dir,save_filename),rot2_flip)
+                cnt+=1
+
+                m3 = cv.getRotationMatrix2D((256, 256), 270, 1)
+                rot3 = cv.warpAffine(patch, m3, (512, 512))
+                save_filename = prefixes[c] + i2str(i + 1) + '_p' + i2str(cnt) + '.png'
+                cv.imwrite(os.path.join(save_dir,save_filename),rot3)
+                cnt+=1
+
+                rot3_flip = cv.flip(rot3, 1)
+                save_filename = prefixes[c] + i2str(i + 1) + '_p' + i2str(cnt) + '.png'
+                cv.imwrite(os.path.join(save_dir,save_filename),rot3_flip)
+                cnt+=1
+
