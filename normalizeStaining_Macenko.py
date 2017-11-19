1ba4cf6a9438d8ab6f29ed82e73d452dfde2c383
PeterB <byfield554@gmail.com>
2017-11-19 22:09:47 +0000

    before

--- normalizeStaining_Macenko.py
+++ normalizeStaining_Macenko.py
@@ -1,9 +1,19 @@
 import numpy as np
 
 
+def normalize_columns(A):
+    temp = np.zeros(A.shape)
+    n = A.shape[1]
+    for i in range(n):
+        v = A[:, i]
+        v = v / np.linalg.norm(v)
+        temp[:, i] = v
+    return temp
+
+
 def normalizeStaining(I):
     """
-    Stain normalization via the method of:
+    Stain normalization based on the method of:
 
     M. Macenko et al., ‘A method for normalizing histology slides for quantitative analysis’, in 2009 IEEE International Symposium on Biomedical Imaging: From Nano to Macro, 2009, pp. 1107–1110.
 
@@ -14,59 +24,65 @@ def normalizeStaining(I):
     :param I:
     :return:
     """
+    mask = (I == 0)
+    I[mask] = 1
+    I = I.astype(np.float32)
 
-    Io = 240
+    Io = 255.0  # 240?
     beta = 0.15
-    alpha = 1
+    alpha = 5
 
-    HERef = np.array([[0.5626, 0.2159], [0.7201, 0.8012], [0.4062, 0.5581]])
-    maxCRef = np.array([1.9705, 1.0308])
+    HE_Ref = np.array([[0.5626, 0.7201, 0.4062], [0.2159, 0.8012, 0.5581]]).T
+    HE_Ref = normalize_columns(HE_Ref)
+    print(HE_Ref)
 
     (h, w, c) = np.shape(I)
     I = np.reshape(I, (h * w, c))
 
-    OD = - np.log((I + 1) / Io)
+    OD = - np.log(I / Io)
     ODhat = (OD[(np.logical_not((OD < beta).any(axis=1))), :])
 
     _, V = np.linalg.eigh(np.cov(ODhat, rowvar=False))
     V = V[:, [2, 1, 0]]
+    V = V[:, [0, 1]]
+    if V[0, 0] < 0: V[:, 0] *= -1
+    if V[0, 1] < 0: V[:, 1] *= -1
 
-    Vec = - np.transpose(np.array([V[:, 1], V[:, 0]]))
-    That = np.dot(ODhat, Vec)
+    That = np.dot(ODhat, V)
     phi = np.arctan2(That[:, 1], That[:, 0])
     minPhi = np.percentile(phi, alpha)
     maxPhi = np.percentile(phi, 100 - alpha)
-    vMin = np.dot(Vec, np.array([np.cos(minPhi), np.sin(minPhi)]))
-    vMax = np.dot(Vec, np.array([np.cos(maxPhi), np.sin(maxPhi)]))
-    if vMin[0] > vMax[0]:
-        HE = np.array([vMin, vMax])
-    else:
-        HE = np.array([vMax, vMin])
 
-    HE = np.transpose(HE)
-    Y = np.transpose(OD)
+    vE = np.dot(V, np.array([np.cos(minPhi), np.sin(minPhi)]))
+    vH = np.dot(V, np.array([np.cos(maxPhi), np.sin(maxPhi)]))
 
-    C = np.linalg.lstsq(HE, Y)
-    maxC = np.percentile(C[0], 99, axis=1)
+    HE = np.array([vH, vE]).T
+    HE = normalize_columns(HE)
 
-    C = C[0] / maxC[:, None]
-    C = C * maxCRef[:, None]
-    Inorm = Io * np.exp(- np.dot(HERef, C))
+    Y = OD.T
+    C = np.linalg.lstsq(HE, Y)[0]
+
+    # maxC_Ref = np.array([1.9705, 1.0308]).reshape((2, 1))
+    # maxC = np.percentile(C, 99, axis=1).reshape((2, 1))
+    # C = C * maxC_Ref / maxC
+
+    Inorm = Io * np.exp(- np.dot(HE_Ref, C))
+
     Inorm = np.reshape(np.transpose(Inorm), (h, w, c))
     Inorm = np.clip(Inorm, 0, 255)
     Inorm = np.array(Inorm, dtype=np.uint8)
+
     return Inorm
 
 
-import cv2 as cv
-import matplotlib.pyplot as plt
-
-I = cv.cvtColor(cv.imread('im1.tif'), cv.COLOR_BGR2RGB)
-plt.figure(1)
-plt.imshow(I)
-I = np.array(I, dtype=np.uint8)
-I = np.array(I, dtype=np.float64)
-x = normalizeStaining(I)
-plt.figure(2)
-plt.imshow(x)
-plt.show()
+# import cv2 as cv
+# import matplotlib.pyplot as plt
+#
+# I = cv.cvtColor(cv.imread('im1.tif'), cv.COLOR_BGR2RGB)
+# x = normalizeStaining(I)
+#
+# plt.figure(1)
+# plt.imshow(I)
+# plt.figure(2)
+# plt.imshow(x)
+# plt.show()
