fe4f76ddc8b9c5afcfe280e09a1b61a3f29016ae
PeterB <byfield554@gmail.com>
2017-11-20 16:56:45 +0000

    add batch norm

--- CNN_basic.py
+++ CNN_basic.py
@@ -19,11 +19,12 @@ lr = 1e-3
 ######
 
 
-def conv_relu3x3(x, scope, num_filters, padding='valid'):
+def conv_relu3x3(x, scope, num_filters, padding='VALID', train=True):
     with tf.variable_scope(scope):
         h1 = tf.layers.conv2d(x, filters=num_filters, kernel_size=(3, 3), strides=(1, 1), padding=padding, name='conv',
                               kernel_initializer=tf.contrib.layers.xavier_initializer())
-        return tf.nn.relu(h1)
+        h2 = tf.layers.batch_normalization(h1, scale=True, center=True, name='bn', training=train, momentum=0.9)
+        return tf.nn.relu(h2)
 
 
 def max_pool3x3(x):
@@ -34,11 +35,12 @@ def max_pool2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
 
-def dense_relu(x, scope, num_out):
+def dense_relu(x, scope, num_out, train=True):
     with tf.variable_scope(scope):
         h1 = tf.layers.dense(x, units=num_out, activation=None, name='fc',
                              kernel_initializer=tf.contrib.layers.xavier_initializer())
-        return tf.nn.relu(h1)
+        h2 = tf.layers.batch_normalization(h1, scale=True, center=True, name='bn', training=train, momentum=0.9)
+        return tf.nn.relu(h2)
 
 
 def dense(x, scope, num_out):
@@ -53,27 +55,30 @@ def dense(x, scope, num_out):
 tf.reset_default_graph()
 x = tf.placeholder(tf.float32, [None, 512, 512, 3])
 y = tf.placeholder(tf.uint8, [None, 4])
+training = tf.placeholder(tf.bool, name='train')
 
-l1 = conv_relu3x3(x, 'conv1', 16)
+l1 = conv_relu3x3(x, 'conv1', 16, train=training)
 l2 = max_pool3x3(l1)
-l3 = conv_relu3x3(l2, 'conv2', 32)
+l3 = conv_relu3x3(l2, 'conv2', 32, train=training)
 l4 = max_pool2x2(l3)
-l5 = conv_relu3x3(l4, 'conv3', 64, padding='same')
+l5 = conv_relu3x3(l4, 'conv3', 64, padding='same', train=training)
 l6 = max_pool2x2(l5)
-l7 = conv_relu3x3(l6, 'conv4', 64, padding='same')
+l7 = conv_relu3x3(l6, 'conv4', 64, padding='same', train=training)
 l8 = max_pool3x3(l7)
-l9 = conv_relu3x3(l8, 'conv5', 32)
+l9 = conv_relu3x3(l8, 'conv5', 32, train=training)
 l10 = max_pool3x3(l9)
 flat = tf.contrib.layers.flatten(l10)
-f1 = dense_relu(flat, 'fc1', 256)
-f2 = dense_relu(f1, 'fc2', 128)
+f1 = dense_relu(flat, 'fc1', 256, train=training)
+f2 = dense_relu(f1, 'fc2', 128, train=training)
 out = dense(f2, 'fc3', 4)
 
 accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(out, 1)), 'float32'))
 
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=out))
 
-train_step = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
+update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(update_ops):
+    train_step = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
 
 ######
 
@@ -83,7 +88,7 @@ sess.run(tf.global_variables_initializer())
 # Check we can get 100% accuracy on training set!
 for i in range(100):
     print('i={}'.format(i))
-    sess.run(train_step, feed_dict={x: X, y: Y})
+    sess.run(train_step, feed_dict={x: X, y: Y, training: True})
 
     if i % 10 == 0:
-        print(sess.run(accuracy, {x: X, y: Y}))
+        print(sess.run(accuracy, {x: X, y: Y, training: False}))
