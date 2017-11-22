1ccd05afee369c0764f81ab519cde138d92f620d
PeterB <byfield554@gmail.com>
2017-11-22 17:20:30 +0000

    batch utils

--- batch_utils.py
+++ batch_utils.py
@@ -13,16 +13,15 @@ def read_and_decode(filename_queue):
                                                                      })
 
     image = tf.decode_raw(features['image_raw'], tf.uint8)
-    label = tf.cast(features['label'], tf.int32)
-
     height = tf.cast(features['height'], tf.int32)
     width = tf.cast(features['width'], tf.int32)
     image = tf.reshape(image, [height, width, 3])
 
+    label = tf.cast(features['label'], tf.int32)
+
     # Transformations can be put here.
 
-
-    images, labels = tf.train.shuffle_batch([image, label], batch_size=16, capacity=30, num_threads=2,
+    images, labels = tf.train.shuffle_batch([image, label], batch_size=2, capacity=30, num_threads=1,
                                             min_after_dequeue=10)
 
     return images, labels
