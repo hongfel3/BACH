4f7c929f68018c19adf37eb99d095f1034b3c4d9
PeterB <byfield554@gmail.com>
2017-11-22 18:13:27 +0000

    pretty

--- get_tfrecords.py
+++ get_tfrecords.py
@@ -23,9 +23,6 @@ def _int64_feature(value):
 
 perm1 = np.random.permutation(100)
 
-tfrecords_filename = os.path.join(save_dir, 'training.tfrecords')
-os.makedirs(os.path.dirname(tfrecords_filename), exist_ok=True)
-
 file_names = []
 file_labels = []
 
@@ -36,9 +33,14 @@ for c in classes:
                 os.path.join(data_dir, c, prefixes[c] + mu.i2str(i + 1) + '_patch' + mu.i2str(j + 1) + '.png'))
             file_labels.append(labels[c])
 
+###
+
 total = 4 * 80 * 35
 stop = int(0.75 * total)
 perm2 = np.random.permutation(total)
+
+tfrecords_filename = os.path.join(save_dir, 'training.tfrecords')
+os.makedirs(os.path.dirname(tfrecords_filename), exist_ok=True)
 
 writer = tf.python_io.TFRecordWriter(tfrecords_filename)
 
