31e26d51205de612da8e1e4bc41d53846a12e95d
PeterB <byfield554@gmail.com>
2017-11-22 15:27:15 +0000

    tfrecords

--- get_tfrecords.py
+++ get_tfrecords.py
@@ -3,8 +3,8 @@ import misc_utils as mu
 import tensorflow as tf
 import os
 
-data_dir = '/home/peter/datasets/BACH_patches'
-save_dir = 'home/peter/datasets/BACH_tfrecords'
+data_dir = '/home/peter/datasets/ICIAR2018_BACH_Challenge/BACH_patches'
+save_dir = 'home/peter/datasets/ICIAR2018_BACH_Challenge/BACH_tfrecords'
 
 classes = ('Benign', 'InSitu', 'Invasive', 'Normal')
 labels = {'Benign': 0, 'InSitu': 1, 'Invasive': 2, 'Normal': 3}
@@ -53,28 +53,28 @@ writer.close()
 
 ###
 
-tfrecords_filename = os.path.join(save_dir,'testing.tfrecords')
-os.makedirs(os.path.dirname(tfrecords_filename), exist_ok=True)
-
-writer = tf.python_io.TFRecordWriter(tfrecords_filename)
-
-for c in classes:
-    for i in perm[80:]:
-        print('Doing image {}'.format(i))
-        for j in range(35):
-            path = os.path.join(data_dir, c, prefixes[c] + mu.i2str(i + 1) + '_patch' + mu.i2str(j + 1) + '.png')
-            image = mu.read_image(path)
-
-            height, width, _ = image.shape
-
-            image_raw = image.tostring()
-
-            example = tf.train.Example(features=tf.train.Features(feature={
-                'height': _int64_feature(height),
-                'width': _int64_feature(width),
-                'image_raw': _bytes_feature(image_raw),
-                'label': _int64_feature(labels[c])}))
-
-            writer.write(example.SerializeToString())
-
-writer.close()
+# tfrecords_filename = os.path.join(save_dir,'testing.tfrecords')
+# os.makedirs(os.path.dirname(tfrecords_filename), exist_ok=True)
+#
+# writer = tf.python_io.TFRecordWriter(tfrecords_filename)
+#
+# for c in classes:
+#     for i in perm[80:100]:
+#         print('Doing image {}'.format(i))
+#         for j in range(35):
+#             path = os.path.join(data_dir, c, prefixes[c] + mu.i2str(i + 1) + '_patch' + mu.i2str(j + 1) + '.png')
+#             image = mu.read_image(path)
+#
+#             height, width, _ = image.shape
+#
+#             image_raw = image.tostring()
+#
+#             example = tf.train.Example(features=tf.train.Features(feature={
+#                 'height': _int64_feature(height),
+#                 'width': _int64_feature(width),
+#                 'image_raw': _bytes_feature(image_raw),
+#                 'label': _int64_feature(labels[c])}))
+#
+#             writer.write(example.SerializeToString())
+#
+# writer.close()
