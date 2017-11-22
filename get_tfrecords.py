c75fa5b6207ffccd456174a92eced2438b1d0e8a
PeterB <byfield554@gmail.com>
2017-11-22 14:57:35 +0000

    tfrecords

--- get_tfrecords.py
+++ get_tfrecords.py
@@ -1,8 +1,10 @@
 import misc_utils as mu
 import tensorflow as tf
 import os
+import numpy as np
 
 data_dir = '/home/peter/datasets/BACH_patches'
+save_dir = 'home/peter/datasets/BACH_tfrecords'
 
 classes = ('Benign', 'InSitu', 'Invasive', 'Normal')
 labels = {'Benign': 0, 'InSitu': 1, 'Invasive': 2, 'Normal': 3}
@@ -19,12 +21,42 @@ def _int64_feature(value):
 
 ###
 
-tfrecords_filename = 'dataset.tfrecords'
+perm=np.random.permutation(100)
+
+###
+
+tfrecords_filename = os.path.join(save_dir,'training.tfrecords')
 
 writer = tf.python_io.TFRecordWriter(tfrecords_filename)
 
 for c in classes:
-    for i in range(1):
+    for i in perm[0:80]:
+        for j in range(35):
+            path = os.path.join(data_dir, c, prefixes[c] + mu.i2str(i + 1) + '_patch' + mu.i2str(j + 1) + '.png')
+            image = mu.read_image(path)
+
+            height, width, _ = image.shape
+
+            image_raw = image.tostring()
+
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'height': _int64_feature(height),
+                'width': _int64_feature(width),
+                'image_raw': _bytes_feature(image_raw),
+                'label': _int64_feature(labels[c])}))
+
+            writer.write(example.SerializeToString())
+
+writer.close()
+
+###
+
+tfrecords_filename = os.path.join(save_dir,'testing.tfrecords')
+
+writer = tf.python_io.TFRecordWriter(tfrecords_filename)
+
+for c in classes:
+    for i in perm[80:]:
         for j in range(35):
             path = os.path.join(data_dir, c, prefixes[c] + mu.i2str(i + 1) + '_patch' + mu.i2str(j + 1) + '.png')
             image = mu.read_image(path)
