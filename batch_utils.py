6bdbea124cc6b657ce766e68e52b318cfd3e3e7f
PeterB <byfield554@gmail.com>
2017-11-22 16:01:28 +0000

    batch utils

---
+++ batch_utils.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+
+def read_and_decode(filename_queue):
+    reader = tf.TFRecordReader()
+
+    _, serialized_example = reader.read(filename_queue)
+
+    features = tf.parse_single_example(
+        serialized_example,
+        features={
+            'height': tf.FixedLenFeature([], tf.int64),
+            'width': tf.FixedLenFeature([], tf.int64),
+            'image_raw': tf.FixedLenFeature([], tf.string),
+            'label': tf.FixedLenFeature([], tf.int64)
+        })
+
+    image = tf.decode_raw(features['image_raw'], tf.uint8)
+    label = tf.cast(features['label'], tf.int32)
+
+    height = tf.cast(features['height'], tf.int32)
+    width = tf.cast(features['width'], tf.int32)
+    image_shape = tf.stack([height, width, 3])
+    image = tf.reshape(image, image_shape)
+
+    # Transformations can be put here.
+
+
+    images, labels = tf.train.shuffle_batch([image, label],
+                                                 batch_size=16,
+                                                 capacity=30,
+                                                 num_threads=2,
+                                                 min_after_dequeue=10)
+
+    return images, labels
