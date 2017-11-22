31f93a75f72e885ffa890e601dbef997a764b132
PeterB <byfield554@gmail.com>
2017-11-22 16:31:51 +0000

    batch utils

--- batch_utils.py
+++ batch_utils.py
@@ -6,21 +6,18 @@ def read_and_decode(filename_queue):
 
     _, serialized_example = reader.read(filename_queue)
 
-    features = tf.parse_single_example(
-        serialized_example,
-        features={
-            'height': tf.FixedLenFeature([], tf.int64),
-            'width': tf.FixedLenFeature([], tf.int64),
-            'image_raw': tf.FixedLenFeature([], tf.string),
-            'label': tf.FixedLenFeature([], tf.int64)
-        })
+    features = tf.parse_single_example(serialized_example, features={'height': tf.FixedLenFeature([], tf.int64),
+                                                                     'width': tf.FixedLenFeature([], tf.int64),
+                                                                     'image_raw': tf.FixedLenFeature([], tf.string),
+                                                                     'label': tf.FixedLenFeature([], tf.int64)
+                                                                     })
 
     image = tf.decode_raw(features['image_raw'], tf.uint8)
     label = tf.cast(features['label'], tf.int32)
 
     height = tf.cast(features['height'], tf.int32)
     width = tf.cast(features['width'], tf.int32)
-    image = tf.reshape(image, (height, width, 3))
+    image = tf.reshape(image, [height, width, 3])
 
     # Transformations can be put here.
 
