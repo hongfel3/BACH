5cdd2533c792933e2db80162b0067a5760c9bdaa
PeterB <byfield554@gmail.com>
2017-11-24 11:28:11 +0000

    rgb only

--- get_mean.py
+++ get_mean.py
@@ -1,20 +1,25 @@
 import numpy as np
 import os
 import misc_utils as mu
+import glob
 
-data_dir = '/home/peter/datasets/ICIAR2018_BACH_Challenge/BACH_patches'
+data_dir = '/home/peter/datasets/ICIAR2018_BACH_Challenge/Train_set'
 
 classes = ('Benign', 'InSitu', 'Invasive', 'Normal')
 prefixes = {'Benign': 'b', 'InSitu': 'is', 'Invasive': 'iv', 'Normal': 'n'}
 
 mean = np.zeros((512, 512, 3), dtype=np.float32)
 
+full = []
 for c in classes:
-    for i in range(100):
-        for j in range(35):
-            filename = os.path.join(data_dir, c, prefixes[c] + mu.i2str(i + 1) + '_patch' + mu.i2str(j + 1) + '.png')
-            mean += mu.read_image(filename).astype(np.float32)
+    sub = os.path.join(data_dir, c, '*.png')
+    full += glob.glob(sub)
 
-mean /= (4 * 100 * 35)
+for f in full:
+    print(f)
+    mean += mu.read_image(f).astype(np.float32)
+
+mean=np.reshape(mean,(-1,3))
+mean = np.mean(mean,axis=0)
 
 np.save('./mean.npy', mean)
