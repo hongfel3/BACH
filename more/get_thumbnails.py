cb61781a0a8573ab87a47363aaf4127f2766f902
PeterB <byfield554@gmail.com>
2017-11-24 10:34:11 +0000

    fix

--- more/get_thumbnails.py
+++ more/get_thumbnails.py
@@ -1,5 +1,5 @@
-import cv2 as cv
 import os
+import misc_utils as mu
 
 data_dir = '/home/peter/datasets/ICIAR2018_BACH_Challenge/Photos'
 save_dir = '/home/peter/datasets/ICIAR2018_BACH_Challenge/BACH_thumbnails'
@@ -10,37 +10,15 @@ prefixes = {'Benign': 'b', 'InSitu': 'is', 'Invasive': 'iv', 'Normal': 'n'}
 
 #############################################
 
-def read_image(path):
-    im = cv.imread(path)
-    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
-    return im
-
-
-def i2str(i):
-    s = str(i)
-    if len(s) == 1:
-        return '00' + s
-    elif len(s) == 2:
-        return '0' + s
-    else:
-        return s
-
-
-def save_aspng(im, full_path, compression=3):
-    im = cv.cvtColor(im, cv.COLOR_RGB2BGR)
-    cv.imwrite(full_path, im, [cv.IMWRITE_PNG_COMPRESSION, compression])
-
-
-###########################################
 
 for c in classes:
     for i in range(100):
-        filename = prefixes[c] + i2str(i + 1) + '.tif'
+        filename = prefixes[c] + mu.i2str(i + 1) + '.tif'
         print('Doing Image {}'.format(filename))
         path = os.path.join(data_dir, c, filename)
-        image = read_image(path)
+        image = mu.read_image(path)
 
-        save_filename = prefixes[c] + i2str(i + 1) + '_thumb.png'
+        save_filename = prefixes[c] + mu.i2str(i + 1) + '_thumb.png'
         save_path = os.path.join(save_dir, c, save_filename)
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
-        save_aspng(image, save_path, compression=5)
+        mu.save_aspng(image, save_path, compression=5)
