acb6086796ef368584e059c5de16fbc0ea77b596
PeterB <byfield554@gmail.com>
2017-11-25 12:51:18 +0000

    add mini

--- get_patches.py
+++ get_patches.py
@@ -8,6 +8,8 @@ data_dir = '/home/peter/datasets/ICIAR2018_BACH_Challenge/BACH_normalized'
 save_dir_train = '/home/peter/datasets/ICIAR2018_BACH_Challenge/Train_set'
 save_dir_val = '/home/peter/datasets/ICIAR2018_BACH_Challenge/Val_set'
 save_dir_test = '/home/peter/datasets/ICIAR2018_BACH_Challenge/Test_set'
+save_dir_mini = '/home/peter/datasets/ICIAR2018_BACH_Challenge/Mini_set'
+
 
 classes = ('Benign', 'InSitu', 'Invasive', 'Normal')
 prefixes = {'Benign': 'b', 'InSitu': 'is', 'Invasive': 'iv', 'Normal': 'n'}
@@ -60,3 +62,11 @@ for c in classes:
 
         sub = os.path.join(save_dir_test, c, prefixes[c] + mu.i2str(i + 1))
         mu.save_aspng(image, sub + '.png', compression=1)
+
+for c in classes:
+    filename = prefixes[c] + mu.i2str(1) + '_normalized.png'
+    path = os.path.join(data_dir, c, filename)
+    image = mu.read_image(path)
+
+    sub = os.path.join(save_dir_mini, c, prefixes[c] + mu.i2str(1))
+    get_patches(image, sub)
