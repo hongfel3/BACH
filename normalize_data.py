228e1bcda5eac7101e2883e83cc2a5a0f0f2316a
PeterB <byfield554@gmail.com>
2017-12-03 12:13:46 +0000

    fix

--- normalize_data.py
+++ normalize_data.py
@@ -11,6 +11,8 @@ classes = ('Benign', 'InSitu', 'Invasive', 'Normal')
 prefixes = {'Benign': 'b', 'InSitu': 'is', 'Invasive': 'iv', 'Normal': 'n'}
 
 target = mu.read_image(os.path.join(data_dir, 'Benign', 'b027.tif'))
+n=stain.normalizer()
+n.fit(target)
 
 for c in classes:
     for i in range(100):
@@ -19,7 +21,7 @@ for c in classes:
         path = os.path.join(data_dir, c, filename)
         image = mu.read_image(path)
 
-        normal = stain.normalize_Reinhard(image, target)
+        normal = n.transform(image)
 
         save_filename = prefixes[c] + mu.i2str(i + 1) + '_normalized.png'
         save_path = os.path.join(save_dir, c, save_filename)
