26e75bcc0927b7019085f4efae4a30c9b3656e0e
PeterB <byfield554@gmail.com>
2017-12-03 12:07:20 +0000

    fix

--- utils/stainNorm_Reinhard.py
+++ utils/stainNorm_Reinhard.py
@@ -5,58 +5,53 @@ E. Reinhard, M. Adhikhmin, B. Gooch, and P. Shirley, ‘Color transfer between i
 """
 
 import cv2 as cv
-import numpy as np
+import numpy as np  # Normalise each channel based on statistics of source and target images
 
-def normalize_Reinhard(patch, targetImg):
+
+def lab_split(I):
     """
-    :param patch: a patch RGB in format uint8
-    :param targetImg: a target RGB image in format uint8
+
+    :param I: uint8
     :return:
     """
-
-    # Convert from RGB to Lab color space
-    patch = cv.cvtColor(patch, cv.COLOR_RGB2LAB)
-    patch = patch.astype(np.float32)
-    s1, s2, s3 = cv.split(patch)
-    s1 /= 2.55
-    s2 -= 128.0
-    s3 -= 128.0
-
-    # Mean and Standard Deviation of Source image channels in Lab Colourspace
-    mS1, sdS1 = cv.meanStdDev(s1)
-    mS2, sdS2 = cv.meanStdDev(s2)
-    mS3, sdS3 = cv.meanStdDev(s3)
+    I = cv.cvtColor(I, cv.COLOR_RGB2LAB)
+    I = I.astype(np.float32)
+    I1, I2, I3 = cv.split(I)
+    I1 /= 2.55
+    I2 -= 128.0
+    I3 -= 128.0
+    return I1, I2, I3
 
 
-    # Convert from RGB to Lab color space
-    targetImg = cv.cvtColor(targetImg, cv.COLOR_RGB2LAB)
-    targetImg = targetImg.astype(np.float32)
-    t1, t2, t3 = cv.split(targetImg)
-    t1 /= 2.55
-    t2 -= 128.0
-    t3 -= 128.0
+def merge_back(I1, I2, I3):
+    """
 
-    # Mean and Standard Deviation of Target image channels in Lab Colourspace
-    mT1, sdT1 = cv.meanStdDev(t1)
-    mT2, sdT2 = cv.meanStdDev(t2)
-    mT3, sdT3 = cv.meanStdDev(t3)
+    :param I1:
+    :param I2:
+    :param I3:
+    :return:
+    """
+    I1 *= 2.55
+    I2 += 128.0
+    I3 += 128.0
+    I = cv.merge((I1, I2, I3)).astype(np.uint8)
+    return cv.cvtColor(I, cv.COLOR_LAB2RGB)
 
-    # Normalise each channel based on statistics of source and target images
-    normLab_1 = ((s1 - mS1) * (sdT1 / sdS1)) + mT1
-    normLab_2 = ((s2 - mS2) * (sdT2 / sdS2)) + mT2
-    normLab_3 = ((s3 - mS3) * (sdT3 / sdS3)) + mT3
 
-    # Merge back
-    normLab_1 *= 2.55
-    normLab_2 += 128.0
-    normLab_3 += 128.0
-    normLab = cv.merge((normLab_1, normLab_2, normLab_3))
-    normLab = normLab.astype(np.uint8)
+def get_mean_std(I):
+    """
 
-    # Convert back to RGB
-    norm = cv.cvtColor(normLab, cv.COLOR_LAB2RGB)
+    :param I: uint8
+    :return:
+    """
+    I1, I2, I3 = lab_split(I)
+    m1, sd1 = cv.meanStdDev(I1)
+    m2, sd2 = cv.meanStdDev(I2)
+    m3, sd3 = cv.meanStdDev(I3)
+    means = m1, m2, m3
+    stds = sd1, sd2, sd3
+    return means, stds
 
-    return norm
 
 ###
 
@@ -66,24 +61,18 @@ class normalizer(object):
     """
 
     def __init__(self):
-        self.
+        self.target_means = None
+        self.target_stds = None
 
     def fit(self, target):
-        self.stain_matrix_target = get_stain_matrix(target)
-
-    def target_stains(self):
-        return ut.OD_to_RGB(self.stain_matrix_target)
+        means, stds = get_mean_std(target)
+        self.target_means = means
+        self.target_stds = stds
 
     def transform(self, I):
-        stain_matrix_source = get_stain_matrix(I)
-        source_concentrations = ut.get_concentrations(I, stain_matrix_source)
-        return (255 * np.exp(-1 * np.dot(source_concentrations, self.stain_matrix_target).reshape(I.shape))).astype(
-            np.uint8)
-
-    def hematoxylin(self, I):
-        h, w, c = I.shape
-        stain_matrix_source = get_stain_matrix(I)
-        source_concentrations = ut.get_concentrations(I, stain_matrix_source)
-        H = source_concentrations[:, 0].reshape(h, w)
-        H = np.exp(-1 * H)
-        return H
+        I1, I2, I3 = lab_split(I)
+        means, stds = get_mean_std(I)
+        norm1 = ((I1 - means[0]) * (stds[0] / self.target_stds[0])) + self.target_means[0]
+        norm2 = ((I2 - means[1]) * (stds[1] / self.target_stds[1])) + self.target_means[1]
+        norm3 = ((I3 - means[2]) * (stds[2] / self.target_stds[2])) + self.target_means[2]
+        return merge_back(norm1, norm2, norm3)
