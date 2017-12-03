ffe1113dc52e0153491f585840f3350aaba76594
PeterB <byfield554@gmail.com>
2017-12-03 00:01:58 +0000

    fix

--- utils/stainNorm_Vahadane.py
+++ utils/stainNorm_Vahadane.py
@@ -24,7 +24,7 @@ def remove_zeros(I):
 
 def RGB_to_OD(I):
     """
-
+    Convert from RGB to optical density
     :param I:
     :return:
     """
@@ -34,7 +34,7 @@ def RGB_to_OD(I):
 
 def OD_to_RGB(OD):
     """
-
+    Convert from optical density to RGB
     :param OD:
     :return:
     """
@@ -76,26 +76,29 @@ def sign(x):
         return 0
 
 
-def enforce_rows_positive(X):
-    """
-    Make rows positive if possible. Return is tuple of X and bool (true if success)
-    :param X:
-    :return:
-    """
-    n, l = X.shape
-    for i in range(n):
-        sign0 = sign(X[i, 0])
-        for j in range(1, l):
-            if sign(X[i, j]) != sign0:
-                print('Mixed sign rows.')
-                return X, False
-        X[i] = sign0 * X[i]
-    return X, True
+# def enforce_rows_positive(X):
+#     """
+#     Make rows positive if possible. Return is tuple of X and bool (true if success)
+#     :param X:
+#     :return:
+#     """
+#     n, l = X.shape
+#     for i in range(n):
+#         sign0 = sign(X[i, 0])
+#         for j in range(1, l):
+#             if sign(X[i, j]) != sign0:
+#                 print('Mixed sign rows.')
+#                 return X, False
+#         X[i] = sign0 * X[i]
+#     return X, True
+
+
+####
 
 
 def get_stain_matrix(I, threshold=0.8, sub_sample=10000):
     """
-
+    Get 2x3 stain matrix. First row H and second row E
     :param I:
     :param threshold:
     :param sub_sample:
@@ -107,33 +110,43 @@ def get_stain_matrix(I, threshold=0.8, sub_sample=10000):
     n = OD.shape[0]
     if n > sub_sample:
         OD = OD[np.random.choice(range(n), sub_sample)]
-    dictionary = spams.trainDL(OD.T, K=2, lambda1=.1, mode=2, modeD=0, posAlpha=True, posD=True).T
-    print(dictionary.shape)
+    dictionary = spams.trainDL(OD.T, K=2, lambda1=1, mode=2, modeD=0, posAlpha=True, posD=True).T
     if dictionary[0, 0] < dictionary[1, 0]:
         dictionary = dictionary[[1, 0], :]
     dictionary = normalize_rows(dictionary)
     return dictionary
 
 
-def get_coeffs(I, stain_matrix):
+def get_concentrations(I, stain_matrix):
     """
-
+    Get concentrations, a npix x 2 matrix
     :param I:
     :param stain_matrix:
     :return:
     """
     OD = RGB_to_OD(I).reshape((-1, 3))
-    return spams.lasso(OD.T, D=stain_matrix.T, mode=2, lambda1=.1, pos=True).toarray().T
+    return spams.lasso(OD.T, D=stain_matrix.T, mode=2, lambda1=0.1, pos=True).toarray().T
 
 
-def normalize_Vahadane(patch, targetImg):
+def normalize_Vahadane(I, targetImg):
     """
-
-    :param patch:
+    Normalize a patch  to stain of target
+    :param I:
     :param targetImg:
     :return:
     """
-    stain_matrix_source = get_stain_matrix(patch)
+    stain_matrix_source = get_stain_matrix(I)
     stain_matrix_target = get_stain_matrix(targetImg)
-    source_concentrations = get_coeffs(patch, stain_matrix_source)
-    return 255 * np.exp(-1 * np.dot(source_concentrations, stain_matrix_target).reshape(patch.shape))
+    source_concentrations = get_concentrations(I, stain_matrix_source)
+    return (255 * np.exp(-1 * np.dot(source_concentrations, stain_matrix_target).reshape(I.shape))).astype(np.uint8)
+
+
+def HandE(I):
+    h, w, c = I.shape
+    stain_matrix_source = get_stain_matrix(I)
+    source_concentrations = get_concentrations(I, stain_matrix_source)
+    H = source_concentrations[:, 0].reshape(h, w)
+    H = np.exp(-1 * H)
+    E = source_concentrations[:, 1].reshape(h, w)
+    E = np.exp(-1 * E)
+    return H, E
