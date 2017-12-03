08877d3c200505f22c6063e58dc3bba43a4bd45e
PeterB <byfield554@gmail.com>
2017-12-03 11:32:46 +0000

    fix

--- utils/stainNorm_Macenko.py
+++ utils/stainNorm_Macenko.py
@@ -12,6 +12,7 @@ Use with python via e.g https://anaconda.org/conda-forge/python-spams
 
 import numpy as np
 import spams
+import stainNorm_utils as ut
 
 
 def get_stain_matrix(I, beta=0.15, alpha=1):
@@ -22,13 +23,13 @@ def get_stain_matrix(I, beta=0.15, alpha=1):
     :param alpha:
     :return:
     """
-    OD = RGB_to_OD(I).reshape((-1, 3))
-    ODhat = (OD[(OD > beta).any(axis=1), :])
-    _, V = np.linalg.eigh(np.cov(ODhat, rowvar=False))
+    OD = ut.RGB_to_OD(I).reshape((-1, 3))
+    OD = (OD[(OD > beta).any(axis=1), :])
+    _, V = np.linalg.eigh(np.cov(OD, rowvar=False))
     V = V[:, [2, 1]]
     if V[0, 0] < 0: V[:, 0] *= -1
     if V[0, 1] < 0: V[:, 1] *= -1
-    That = np.dot(ODhat, V)
+    That = np.dot(OD, V)
     phi = np.arctan2(That[:, 1], That[:, 0])
     minPhi = np.percentile(phi, alpha)
     maxPhi = np.percentile(phi, 100 - alpha)
@@ -38,51 +39,50 @@ def get_stain_matrix(I, beta=0.15, alpha=1):
         HE = np.array([v1, v2])
     else:
         HE = np.array([v2, v1])
-    return normalize_rows(HE)
+    return ut.normalize_rows(HE)
 
 
-def get_concentrations(I, stain_matrix):
+def get_concentrations(I, stain_matrix, lamda=0.01):
     """
     Get concentrations, a npix x 2 matrix
     :param I:
     :param stain_matrix:
     :return:
     """
-    OD = RGB_to_OD(I).reshape((-1, 3))
-    return spams.lasso(OD.T, D=stain_matrix.T, mode=2, lambda1=0.01, pos=True).toarray().T
+    OD = ut.RGB_to_OD(I).reshape((-1, 3))
+    return spams.lasso(OD.T, D=stain_matrix.T, mode=2, lambda1=lamda, pos=True).toarray().T
 
 
-####################################################
+###
 
-def normalize_Macenko(patch, targetImg, beta=0.15, alpha=1):
+class normalizer(object):
+    """
+    A stain normalization object
     """
 
-    :param patch: a patch RGB in format uint8
-    :param targetImg: a target RGB image in format uint8
-    :param Io:
-    :param beta:
-    :param alpha:
-    :param intensity_norm:
-    :return:
-    """
+    def __init__(self):
+        self.stain_matrix_target = None
 
-    HE = get_stain_matrix(patch, beta=beta, alpha=alpha)
+    def fit(self, target):
+        self.stain_matrix_target = get_stain_matrix(target)
+        self.target_concentrations = get_concentrations(target, self.stain_matrix_target)
 
-    HE_target = get_stain_matrix(targetImg, beta=beta, alpha=alpha)
+    def target_stains(self):
+        return ut.OD_to_RGB(self.stain_matrix_target)
 
-    # Get source concentrations
-    C = get_concentrations(patch, HE)
+    def transform(self, I):
+        stain_matrix_source = get_stain_matrix(I)
+        source_concentrations = get_concentrations(I, stain_matrix_source)
+        maxC_source = np.percentile(source_concentrations, 99, axis=0).reshape((1, 2))
+        maxC_target = np.percentile(self.target_concentrations, 99, axis=0).reshape((1, 2))
+        source_concentrations *= (maxC_target / maxC_source)
+        return (255 * np.exp(-1 * np.dot(source_concentrations, self.stain_matrix_target).reshape(I.shape))).astype(
+            np.uint8)
 
-    ### Modify concentrations ###
-    maxC = np.percentile(C, 99, axis=0).reshape((1, 2))
-    C_target = get_concentrations(targetImg, HE_target)
-    maxC_target = np.percentile(C_target, 99, axis=0).reshape((1, 2))
-    C = C * maxC_target / maxC
-
-    # Final tidy up
-    Inorm = 255 * np.exp(- np.dot(C, HE_target))
-    Inorm = np.reshape(Inorm, patch.shape)
-    Inorm = np.clip(Inorm, 0, 255)
-    Inorm = np.array(Inorm, dtype=np.uint8)
-
-    return Inorm
+    def hematoxylin(self, I):
+        h, w, c = I.shape
+        stain_matrix_source = get_stain_matrix(I)
+        source_concentrations = get_concentrations(I, stain_matrix_source)
+        H = source_concentrations[:, 0].reshape(h, w)
+        H = np.exp(-1 * H)
+        return H
