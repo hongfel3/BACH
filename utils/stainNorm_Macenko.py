57114e757ba0380658fd99a9677539b037717124
PeterB <byfield554@gmail.com>
2017-12-03 00:16:30 +0000

    change

--- utils/stainNorm_Macenko.py
+++ utils/stainNorm_Macenko.py
@@ -1,4 +1,42 @@
+"""
+Uses the spams package:
+
+http://spams-devel.gforge.inria.fr/index.html
+
+Use with python via e.g https://anaconda.org/conda-forge/python-spams
+"""
+
 import numpy as np
+import spams
+
+
+def remove_zeros(I):
+    """
+    Remove zeros
+    :param I:
+    :return:
+    """
+    mask = (I == 0)
+    I[mask] = 1
+    return I
+
+def normalize_rows(A):
+    """
+    Normalize rows of an array
+    :param A:
+    :return:
+    """
+    return A / np.linalg.norm(A, axis=1)[:, None]
+
+
+def RGB_to_OD(I):
+    """
+    Convert from RGB to optical density
+    :param I:
+    :return:
+    """
+    I = remove_zeros(I)
+    return -1 * np.log(I / 255)
 
 
 def normalize_columns(A):
@@ -10,9 +48,20 @@ def normalize_columns(A):
     return A / np.linalg.norm(A, axis=0)
 
 
+def get_concentrations(I, stain_matrix):
+    """
+    Get concentrations, a npix x 2 matrix
+    :param I:
+    :param stain_matrix:
+    :return:
+    """
+    OD = RGB_to_OD(I).reshape((-1, 3))
+    return spams.lasso(OD.T, D=stain_matrix.T, mode=2, lambda1=0.01, pos=True).toarray().T
+
+
 ####################################################
 
-def normalize_Macenko(patch, targetImg, Io=255, beta=0.15, alpha=1, intensity_norm=True):
+def normalize_Macenko(patch, targetImg, Io=255, beta=0.15, alpha=1):
     """
     Stain normalization based on the method of:
 
@@ -33,19 +82,7 @@ def normalize_Macenko(patch, targetImg, Io=255, beta=0.15, alpha=1, intensity_no
     :param intensity_norm:
     :return:
     """
-
-    # Remove zeros
-    mask = (patch == 0)
-    patch[mask] = 1
-    patch = patch.astype(np.float32)
-    mask = (targetImg == 0)
-    targetImg[mask] = 1
-    targetImg = targetImg.astype(np.float32)
-
-    # Get source stain matrix
-    (h, w, c) = np.shape(patch)
-    patch = np.reshape(patch, (h * w, c))
-    OD = - np.log(patch / Io)
+    OD = RGB_to_OD(patch).reshape((-1,3))
     ODhat = (OD[(OD > beta).any(axis=1), :])
     _, V = np.linalg.eigh(np.cov(ODhat, rowvar=False))
     V = V[:, [2, 1]]
@@ -57,16 +94,14 @@ def normalize_Macenko(patch, targetImg, Io=255, beta=0.15, alpha=1, intensity_no
     maxPhi = np.percentile(phi, 100 - alpha)
     v1 = np.dot(V, np.array([np.cos(minPhi), np.sin(minPhi)]))
     v2 = np.dot(V, np.array([np.cos(maxPhi), np.sin(maxPhi)]))
-    v3 = np.cross(v1, v2)
     if v1[0] > v2[0]:
-        HE = np.array([v1, v2, v3]).T
+        HE = np.array([v1, v2])
     else:
-        HE = np.array([v2, v1, v3]).T
-    HE = normalize_columns(HE)
+        HE = np.array([v2, v1])
+    HE = normalize_rows(HE)
 
     # Get target stain matrix
-    targetImg = np.reshape(targetImg, (-1, 3))
-    OD_target = - np.log(targetImg / Io)
+    OD_target = RGB_to_OD(targetImg).reshape((-1,3))
     ODhat_target = (OD_target[(OD_target > beta).any(axis=1), :])
     _, V = np.linalg.eigh(np.cov(ODhat_target, rowvar=False))
     V = V[:, [2, 1]]
@@ -78,30 +113,28 @@ def normalize_Macenko(patch, targetImg, Io=255, beta=0.15, alpha=1, intensity_no
     maxPhi = np.percentile(phi, 100 - alpha)
     v1 = np.dot(V, np.array([np.cos(minPhi), np.sin(minPhi)]))
     v2 = np.dot(V, np.array([np.cos(maxPhi), np.sin(maxPhi)]))
-    v3 = np.cross(v1, v2)
     if v1[0] > v2[0]:
-        HE_target = np.array([v1, v2, v3]).T
+        HE_target = np.array([v1, v2])
     else:
-        HE_target = np.array([v2, v1, v3]).T
-    HE_target = normalize_columns(HE_target)
+        HE_target = np.array([v2, v1])
+    HE_target = normalize_rows(HE_target)
 
     # Get source concentrations
-    Y = OD.T
-    C = np.linalg.lstsq(HE, Y)[0]
+    C=get_concentrations(patch,HE)
 
-    ### Modify concentrations ###
-    if intensity_norm == True:
-        maxC = np.percentile(C, 99, axis=1).reshape((3, 1))
-        maxC[2] = 1
-        Y_target = OD_target.T
-        C_target = np.linalg.lstsq(HE_target, Y_target)[0]
-        maxC_target = np.percentile(C_target, 99, axis=1).reshape((3, 1))
-        maxC_target[2] = 1
-        C = C * maxC_target / maxC
+    # ### Modify concentrations ### # ignore this
+    # if intensity_norm == True:
+    #     maxC = np.percentile(C, 99, axis=1).reshape((3, 1))
+    #     maxC[2] = 1
+    #     Y_target = OD_target.T
+    #     C_target = np.linalg.lstsq(HE_target, Y_target)[0]
+    #     maxC_target = np.percentile(C_target, 99, axis=1).reshape((3, 1))
+    #     maxC_target[2] = 1
+    #     C = C * maxC_target / maxC
 
     # Final tidy up
-    Inorm = Io * np.exp(- np.dot(HE_target, C))
-    Inorm = np.reshape(np.transpose(Inorm), (h, w, c))
+    Inorm = Io * np.exp(- np.dot(C,HE_target))
+    Inorm = np.reshape(Inorm, (h, w, c))
     Inorm = np.clip(Inorm, 0, 255)
     Inorm = np.array(Inorm, dtype=np.uint8)
 
